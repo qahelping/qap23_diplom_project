3590914489d759f8db3ffd605fd9175304154bb2
Alena Yanushevskaya <qahelping@gmail.vom>
2026-07-07 20:37:06 +0300

    task1

--- services/user_service.py
+++ services/user_service.py
@@ -1,2 +1,21 @@
-class UserService:
-    pass
+import allure
+
+from core.http_client import HttpClient
+from core.logger import logger
+from models.user_models import User, UsersResponse
+
+
+class UserService(HttpClient):
+    def __init__(self):
+        self.url = "/users"
+
+    @allure.step("Get list of user")
+    def get_list_of_user(self):
+        response, status_code = self.get(self.url)
+        logger.info("[USER SERVICE, GET LIST USER] %s", response)
+        return UsersResponse(users=response), status_code
+
+    @allure.step("Get user by id: {user_id}")
+    def get_user_by_id(self, user_id):
+        response = self.get(f"{self.url}/{user_id}")
+        return User(**response)
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,57 +1,64 @@
 import json
 
+import allure
+import pytest
 import requests
 
 from core.get_env import GORES_TOKEN
+from models.user_models import UsersResponse
+from services.user_service import UserService
 from test_data.get_random_body import get_random_user
 from test_data.users_list import USERS_LIST, get_user_by_id
 
 
-def test_get_user():
-    url = "https://gorest.co.in/public/v2/users"
-    response = requests.get(url)
+@allure.suite("User")
+class TestUser:
+    @pytest.mark.user_1
+    @allure.title("Get user")
+    def test_get_user(self):
+        user_service = UserService()
 
-    response_json = response.json()
-    assert response.status_code == 200  # noqa: PLR2004
-    assert response_json == USERS_LIST
+        response, status_code = user_service.get_list_of_user()
+        assert status_code == 200  # noqa: PLR2004
+        assert response == UsersResponse(users=USERS_LIST)
 
+    @allure.title("Get user by id")
+    def test_get_user_by_id(self):
+        user_service = UserService()
+        user_id = "8512085"
 
-def test_get_user_by_id():
-    user_id = "8512085"
-    url = f"https://gorest.co.in/public/v2/users/{user_id}"
-    response = requests.get(url)
+        response = user_service.get_user_by_id(user_id)
 
-    response_json = response.json()
-    assert response.status_code == 200  # noqa: PLR2004
-    assert response_json == get_user_by_id(user_id)
+        assert response.status_code == 200  # noqa: PLR2004
+        assert response == get_user_by_id(user_id)
 
+    @allure.title("Create user")
+    def test_create_user(self):
+        headers = {
+            "Authorization": f"Bearer {GORES_TOKEN}",
+            "Accept": "application/json",
+            "Content-type": "application/json",
+        }
 
-def test_create_user():
-    headers = {
-        "Authorization": f"Bearer {GORES_TOKEN}",
-        "Accept": "application/json",
-        "Content-type": "application/json",
-    }
+        url = "https://gorest.co.in/public/v2/users"
 
-    url = "https://gorest.co.in/public/v2/users"
+        body = get_random_user()
+        response = requests.post(url, headers=headers, data=json.dumps(body))
+        response_json = response.json()
+        assert response.status_code == 201  # noqa: PLR2004
+        assert response_json["name"] == body["name"]
+        assert response_json["email"] == body["email"]
+        assert response_json["gender"] == body["gender"]
+        assert response_json["status"] == body["status"]
 
-    body = get_random_user()
-    response = requests.post(url, headers=headers, data=json.dumps(body))
-    response_json = response.json()
-    assert response.status_code == 201  # noqa: PLR2004
-    assert response_json["name"] == body["name"]
-    assert response_json["email"] == body["email"]
-    assert response_json["gender"] == body["gender"]
-    assert response_json["status"] == body["status"]
+        user_id = response_json["id"]
+        url = f"https://gorest.co.in/public/v2/users/{user_id}"
+        response = requests.get(url, headers=headers)
 
-    user_id = response_json["id"]
-    url = f"https://gorest.co.in/public/v2/users/{user_id}"
-    response = requests.get(url, headers=headers)
+        response_json = response.json()
+        assert response.status_code == 200  # noqa: PLR2004
 
-    response_json = response.json()
-    assert response.status_code == 200  # noqa: PLR2004
-
-    assert response_json["name"] == body["name"]
-    assert response_json["email"] == body["email"]
-    assert response_json["gender"] == body["gender"]
-    assert response_json["status"] == body["status"]
+        assert response_json["name"] == body["name"]
+        assert response_json["email"] == body["email"]
+        assert response_json["gender"] == body["gender"]
+        assert response_json["status"] == body["status"]
